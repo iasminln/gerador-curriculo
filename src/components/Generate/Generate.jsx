de54c5192ca0d02d5a1d5b400f6e5ecc1e5b7540
Iasmin <iasln@outlook.com>
2024-02-02 02:42:51 -0300

    corrected null  code

--- src/components/Generate/Generate.jsx
+++ src/components/Generate/Generate.jsx
@@ -1,23 +1,53 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
 import InputDefault from '../Utils/FormElements/InputDefault';
-import { htmlText } from './Code/Code';
 
 const Generate = () => {
 
+  const getInfo = (item) => {
+    return localStorage.getItem(item)
+  }
+
+  const htmlText = `
+    <!DOCTYPE html>
+    <html lang="pt-BR">
+      <head>
+        <meta charset="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta name="description" content="${getInfo('form-complete-name')} - Currículo" />
+        <title>${getInfo('form-complete-name')} - Currículo</title>
+      
+        <style>
+          main {
+            background: ${getInfo('form-color-background')};
+            color: ${getInfo('form-color-text')};
+            font-size: 22px;
+            font-weight: 700;
+            height: 100vh;
+          }
+        </style>
+      </head>
+  
+      <body>
+        <main>
+          <p>Nome: ${getInfo('form-complete-name')}</p>
+          <p>Telefone: ${getInfo('form-contact-phone')}</p>
+          <p>E-mail: ${getInfo('form-contact-email')}</p>
+        </main>
+      </body>
+    </html>
+  `
+
   const openPreviewWindow = () => {
     const previewWindow = window.open('', '_blank');
-    
+
     if (previewWindow) {
       previewWindow.document.open();
       previewWindow.document.write(htmlText);
       previewWindow.document.close();
     } else {
-      // Tratar o caso em que a janela pop-up foi bloqueada pelo navegador
       alert('A janela pop-up foi bloqueada pelo navegador. Por favor, permita pop-ups para este site.');
     }
   }
-  
 
   return (
     <section className='generate'>
@@ -29,12 +59,8 @@ const Generate = () => {
           value={htmlText}
           readOnly
         />
-
         <button onClick={openPreviewWindow}>Clique para visualizar a página</button>
       </div>
-
-      
-
     </section>
   )
 }
